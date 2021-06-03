49eb8bb90c64851284a80b6e99c5c031d71ca284
Mirnawati Darwis <benangtonggos@gmail.com>
2021-06-03 18:15:39 +0700

    Add Pertemuan 12 (Login & Logout)

---
+++ Kuliah/pertemuan12/functions.php
@@ -0,0 +1,123 @@
+<?php
+
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'p_web');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+
+  $result = mysqli_query($conn, $query);
+
+  // jika hasilnya hanya 1 data
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
+
+function tambah($data)
+{
+  $conn = koneksi();
+
+  $nama = htmlspecialchars($data['Nama']);
+  $nim = htmlspecialchars($data['NIM']);
+  $email = htmlspecialchars($data['Email']);
+  $departemen = htmlspecialchars($data['Departemen']);
+  $gambar = htmlspecialchars($data['Gambar']);
+
+  $query = "INSERT INTO 
+              mahasiswa
+            VALUES 
+              (null, '$nama', '$nim', '$email', '$departemen', '$gambar');
+          ";
+
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+  $conn = koneksi();
+
+  mysqli_query($conn, "DELETE FROM mahasiswa WHERE ID = $id")  or die(mysqli_error($conn));
+
+  return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+  $conn = koneksi();
+
+  $id = $data['id'];
+  $nama = htmlspecialchars($data['Nama']);
+  $nim = htmlspecialchars($data['NIM']);
+  $email = htmlspecialchars($data['Email']);
+  $departemen = htmlspecialchars($data['Departemen']);
+  $gambar = htmlspecialchars($data['Gambar']);
+
+  $query = "UPDATE 
+              mahasiswa
+            SET 
+              Nama = '$nama',
+              NIM = '$nim',
+              Email = '$email',
+              Departemen = '$departemen',
+              Gambar = '$gambar'
+            WHERE ID = $id
+          ";
+
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+
+  return mysqli_affected_rows($conn);
+}
+
+function cari($keyword)
+{
+  $conn = koneksi();
+
+  $query = "SELECT * FROM mahasiswa
+            WHERE Nama LIKE '%$keyword%' OR
+            NIM LIKE '%$keyword%' OR
+            Email LIKE '%$keyword%' OR
+            Departemen LIKE '%$keyword%'";
+
+  $result = mysqli_query($conn, $query);
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
+
+function login($data)
+{
+  $conn = koneksi();
+
+  $username = htmlspecialchars($data['username']);
+  $password = htmlspecialchars($data['password']);
+
+  if (Query("SELECT * FROM user WHERE Username = '$username' && Password = '$password'")) {
+
+    // set session
+    $_SESSION['login'] = true;
+    header("Location: index.php");
+    exit;
+  } else {
+    return [
+      'error' => true,
+      'pesan' => 'Username / Password Salah !'
+    ];
+  }
+}
---
+++ Kuliah/pertemuan12/ubah.php
@@ -0,0 +1,88 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+
+// jika tidak ada id di URL
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+// ambil id dari URL
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE ID = $id");
+
+// cek apakah tombol ubah telah di tekan
+if (isset($_POST['ubah'])) {
+  if (ubah($_POST) > 0) {
+    echo "<script>
+            alert('Data Berhasil Diubah !');
+            document.location.href = 'index.php';
+          </script>";
+  } else {
+    echo "Data Gagal Diubah !!";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ubah Data Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Form Ubah Data Mahasiswa</h3>
+  <form action="" method="POST">
+    <input type="hidden" name="id" value="<?= $m['ID']; ?>">
+    <ul>
+      <li>
+        <label>
+          Nama :
+          <input type="text" name="Nama" autofocus required value="<?= $m['Nama']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          NIM :
+          <input type="text" name="NIM" required value="<?= $m['NIM']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Email :
+          <input type="text" name="Email" required value="<?= $m['Email']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Departemen :
+          <input type="text" name="Departemen" required value="<?= $m['Departemen']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Gambar :
+          <input type="text" name="Gambar" required value="<?= $m['Gambar']; ?>">
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="ubah">Ubah Data</button>
+      </li>
+    </ul>
+  </form>
+</body>
+
+</html>
