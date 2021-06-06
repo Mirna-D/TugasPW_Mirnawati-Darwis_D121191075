906e34391dc83f321d4ecebd6f0bf8cd2c97a73e
Mirnawati Darwis <benangtonggos@gmail.com>
2021-06-06 14:06:44 +0700

    Add Pertemuan 13 (Upload File Gambar)

--- Kuliah/pertemuan13/tambah.php
+++ Kuliah/pertemuan13/tambah.php
@@ -33,7 +33,7 @@ if (isset($_POST['tambah'])) {
 
 <body>
   <h3>Form Tambah Data Mahasiswa</h3>
-  <form action="" method="POST">
+  <form action="" method="POST" enctype="multipart/form-data">
     <ul>
       <li>
         <label>
@@ -62,7 +62,7 @@ if (isset($_POST['tambah'])) {
       <li>
         <label>
           Gambar :
-          <input type="text" name="Gambar" required>
+          <input type="file" name="Gambar">
         </label>
       </li>
       <li>
